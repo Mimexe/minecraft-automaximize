f75f87e8a24dd2772cce850ab57add47f33cb4c2
Mimexe <mime@mimedev.fr>
2025-08-27 21:10:49 +0200

    Remove unused import for FMLClientSetupEvent in Automaximize.java

--- src/main/java/fr/mimedev/automaximize/Automaximize.java
+++ src/main/java/fr/mimedev/automaximize/Automaximize.java
@@ -8,7 +8,6 @@ import net.minecraftforge.client.event.ScreenEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 
